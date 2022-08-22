2795e74eee2ea159e0f1728c757c835acd8b01e4
kader009 <kadermollarasel000@gmail.com>
2022-08-23 01:24:43 +0600

    add some js code and function

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 
+
+// first get player here
+
 const Myplayer = [];
+
+function Collect(NewPlayerName){
+  // console.log(NewPlayerName); 
+
+  
+  const NewCreate = document.getElementById('create');
+  
+  NewCreate.innerHTML = '';
+  
+  for(let i = 0; i < NewPlayerName.length; i++){
+    // console.log(Myplayer[i].getPlayers);
+    const Findname = Myplayer[i].getPlayers;
+    // console.log(Findname);
+    const ol = document.createElement('ol');
+    ol.style.color = 'white'
+    ol.innerHTML = `
+    <ol>
+    <li>${Findname } </li>
+    </ol>
+    
+    `;
+    
+    NewCreate.appendChild(ol);
+    
+
+  }
+    
+
+}
+
+
+// get player value
 
 function PickName(element){
   // console.log(element.parentNode.children);
@@ -10,11 +45,45 @@ function PickName(element){
     getPlayers: PlayerName,
   }
 
-  Myplayer.push(NewPlayers)
+  Myplayer.push(NewPlayers);
 
-  console.log(Myplayer.length);
+  Collect(Myplayer);
+
+  document.getElementById('select').innerText = Myplayer.length;
+
+  // console.log(Myplayer.length);
   // console.log(NewPlayers);
 }
+
+
+// add addeventlistener
+
+document.getElementById('calculate').addEventListener('click', function(){
+  // console.log('you click it');
+  const InputValue = document.getElementById('budgetCal');
+  const PlayerValue = document.getElementById('create');
+
+  const NewValue = InputValue * PlayerValue;
+  console.log(NewValue);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
